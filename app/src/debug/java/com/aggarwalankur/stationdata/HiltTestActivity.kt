af8c49cacc3162feb86c32a8392cb81fbb031a97
Ankur Aggarwal <roc.windwarrior@gmail.com>
2022-01-28 17:48:53 +0100

    setup - Add hilt to the app - Added Hilt plugin + dependencies - Added Hilt application - Added HiltTestRunner and HiltExt. These could be useful in future while running Automation Tests

---
+++ app/src/debug/java/com/aggarwalankur/stationdata/HiltTestActivity.kt
@@ -0,0 +1,7 @@
+package com.aggarwalankur.stationdata
+
+import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class HiltTestActivity : AppCompatActivity()
---
+++ app/src/main/java/com/aggarwalankur/stationdata/StationDataApplication.kt
@@ -0,0 +1,7 @@
+package com.aggarwalankur.stationdata
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class StationDataApplication : Application()
